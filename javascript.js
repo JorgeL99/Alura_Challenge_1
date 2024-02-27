0bff0799760c61b3f18d3c4eb32bf2392c5ee38e
JorgeL99 <sairachambi@gmail.com>
2024-02-26 22:27:44 -0500

    que

--- javascript.js
+++ javascript.js
@@ -5,6 +5,8 @@ function encriptar(){
                             .replace(/e/gi,"enter")
                             .replace(/i/gi,"imes")
                             .replace(/a/gi,"ai")
+                            .replace(/o/gi,"ober")
+                            .replace(/u/gi,"ufat")
 
 
 }
